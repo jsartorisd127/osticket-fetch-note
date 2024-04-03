dfeac161f51bc486574f59afd5f8536f065bc2b1
jsartorisd127 <37547635+jsartorisd127@users.noreply.github.com>
2024-04-03 07:51:54 -0500

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 
 return array(
     'id' =>             'osticket:fetch-note', 
-    'version' =>        '1.0.0',
+    'version' =>        '1.0.0fork',
     'name' =>           'Fetch Note',
-    'author' =>         'Bastian Konetzny',
+    'author' =>         'Bastian Konetzny, fork by jsartorisD127',
     'description' =>    'Fetch content for an issue note on ticket creation.',
-    'url' =>            'https://github.com/bkonetzny/osticket-fetch-note',
+    'url' =>            'https://github.com/jsartorisd127/osticket-fetch-note',
     'plugin' =>         'fetch-note.php:FetchNotePlugin',        
 );
